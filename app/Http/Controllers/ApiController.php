7bb9ec83a0236d65a5b1f70b49361b4676b25858
Lina Brunken <linatt@hotmail.de>
2018-08-07 13:49:51 +0200

    api gebaut

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service;
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class ApiController extends Controller
+{
+
+  public function sendRequestToBService()
+  {
+
+    $answera = 'Antwort vom A-Service!';
+    $client = new Client(); //GuzzleHttp\Client
+    $result = $client->get('http://b-service.homestead/answerToA');
+    return $result;
+  }
+}
