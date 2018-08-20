748f2bd53d582eb92f94cbbf5da74fa53db741a6
Lina Brunken <linatt@hotmail.de>
2018-08-20 11:03:30 +0200

    hurra

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use App\Service;
 use Illuminate\Http\Request;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
 
 class ApiController extends Controller
 {
@@ -17,7 +18,7 @@ class ApiController extends Controller
 
     $answera = 'Antwort vom A-Service!';
     $client = new Client(); //GuzzleHttp\Client
-    $result = $client->post('http://b-service.homestead/answerToA', ['token' => $token])->getBody()->read(128);
-    return array($answera, $token);
+    $result = $client->post('http://b-service.homestead/answerToA', ['json' => ['token' => $token]])->getBody()->read(128);
+    return array($answera, $result);
   }
 }
